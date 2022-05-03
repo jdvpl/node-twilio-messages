a5ccd3a1b39d62cb9ed6a32f6129cefb7e6786d5
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-03 15:41:10 -0500

    funcionando el envio de mensajes de texto

--- routes/sms.routes.js
+++ routes/sms.routes.js
@@ -9,7 +9,7 @@ const router=Router();
 router.post('/',[
   check('numberTo',"No es un numero de telefono").isMobilePhone(),
   check('numberTo',"La numero de telefono es obligatorio").not().isEmpty(),
-  // check('numberFrom',"No es un numero de telefono").isMobilePhone(),
+  check('numberFrom',"No es un numero de telefono").isMobilePhone(),
   check('numberFrom',"La numero de telefono es obligatorio").not().isEmpty(),
   check('message',"El mensaje es obligatorio").not().isEmpty(),
   validarCampos
