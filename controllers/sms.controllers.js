e212c4a07a5229db553bf88e0c20a0dc093d6e56
juan daniel suarez amado <62775732+jdvpl@users.noreply.github.com>
2022-05-03 15:04:04 -0500

    sms

--- controllers/sms.controllers.js
+++ controllers/sms.controllers.js
@@ -1,13 +1,19 @@
 const { response } = require("express");
+const twilio = require("twilio")(process.env.SID_ACCOUNT,process.env.AUTH_TOKEN);
+
 
 
 const sendMessage=async(req,res=response)=>{
   const {numberTo,numberFrom, message} =req.body;
 
   try {
-
+    const response=await twilio.messages.create({
+      from:numberFrom,
+      to:numberTo,
+      body:message
+    })
     
-    res.json({numberTo,numberFrom,message})
+    res.json({numberTo,numberFrom,message,response})
   } catch (error) {
     console.log(error)
     res.status(500).json({msg: `Hubo un error al enviar mensaje`, error})
